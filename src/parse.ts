ba908650ac54de801a494967ba49666174e9c6f6
Steve Konves <stephen.konves@gmail.com>
2025-02-18 15:42:13 -0700

    Upgrade to basketry alpha and implement doc indices

--- src/parse.ts
+++ src/parse.ts
@@ -2,5 +2,7 @@ import { AST } from '.';
 import { parse as parseJson } from './json';
 import { parse as parseYaml } from './yaml';
 
-export const parse: AST.ParseFunction = (doc) =>
-  doc.trimStart().startsWith('{') ? parseJson(doc) : parseYaml(doc);
+export const parse: AST.ParseFunction = (documentIndex, doc) =>
+  doc.trimStart().startsWith('{')
+    ? parseJson(documentIndex, doc)
+    : parseYaml(documentIndex, doc);
